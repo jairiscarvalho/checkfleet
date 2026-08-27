c82ed679bb76dd9d612ea01764001a8ff20749a8
jairiscarvalho <jairisgamer@gmail.com>
2026-08-27 08:50:44 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="checkfleet-pwa-v1";
+const APP=["./","./index.html","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{const u=new URL(e.request.url); if(u.origin===location.origin){e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return res;}).catch(()=>caches.match("./index.html"))));}});
